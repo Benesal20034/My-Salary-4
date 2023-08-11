cd85c0013cd5083afeb043b9771d61007e057062
Ben.issa <107337672+Benesal20034@users.noreply.github.com>
2023-08-12 00:31:20 +0200

    Initial Commit

---
+++ My Salary 2/My_Salary_2App.swift
@@ -0,0 +1,17 @@
+//
+//  My_Salary_2App.swift
+//  My Salary 2
+//
+//  Created by Ben.issa on 12/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_Salary_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
